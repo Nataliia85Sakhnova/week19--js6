8d5d81476ec3774e0574ffe7b0843614c987b439
Razhatawka <nataliia85sakhnova@gmail.com>
2022-11-13 08:27:32 +0200

    работает, не прописана проверка на пустоту

--- week19,JS6, task1 - worker/index.js
+++ week19,JS6, task1 - worker/index.js
@@ -3,42 +3,42 @@
 // Также класс должен иметь метод getSalary(), который будет выводить зарплату работника.
 // Зарплата - это произведение(умножение) ставки rate на количество отработанных дней days.
 
-class Worker {
-
-    constructor(name, surname, rate, days) {
-        this.name = name;
-        this.surname = surname;
-        this.rate = rate;
-        this.days = days;
-    }
-
-    static getSalary(rate, days) {
-        return rate * days;
-    }
-}
-
-//функция проверки инпута на пустоту:
-const isEmpty = (input) => {
-    // if value of input == 0
-    // тогда:
-    // прервать подсчеты и 
-    // document.getElementById("alarm").textContent = "Пожалуйста, заполните все поля!";
-
-}
-
-// создаем переменные, вкладываем в них значение инпутов:
-const namefromvalue = document.getElementById("name").value;
-const surnamefromvalue = document.getElementById("surname").value;
-const ratefromvalue = document.getElementById("rate").value;
-const daysfromvalue = document.getElementById("days").value;
-
-
-
-const worker = new Worker(namefromvalue, surnamefromvalue, ratefromvalue, daysfromvalue); //в какой момент оно должно запуститься
-
-console.log(worker);
 
 document.getElementById("button").addEventListener("click", () => {
-    document.getElementById("salary").textContent = new Worker + "евро";
+
+    class Worker {
+
+        constructor(name, surname, rate, days) {
+            this.name = name;
+            this.surname = surname;
+            this.rate = rate;
+            this.days = days;
+        }
+
+        static getSalary(rate, days) {
+            return rate * days;
+        }
+    }
+
+    //проверка инпута на пустоту:
+    const isEmpty = (input) => {
+        // if value of input == 0;
+        // document.getElementById("alarm").textContent = "Пожалуйста, заполните все поля!";
+
+    }
+
+
+    // создаем переменные, вкладываем в них значение инпутов:
+    const namefromvalue = document.getElementById("name").value;
+    const surnamefromvalue = document.getElementById("surname").value;
+    const ratefromvalue = document.getElementById("rate").value;
+    const daysfromvalue = document.getElementById("days").value;
+
+    const worker = new Worker(namefromvalue, surnamefromvalue, ratefromvalue, daysfromvalue); //в какой момент оно должно запуститься
+
+    console.log(worker);
+
+    document.getElementById("salary").textContent = String(ratefromvalue * daysfromvalue) + " евро";
+
 
 })
