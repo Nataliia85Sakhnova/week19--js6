e0e218b4d3b715bdc781f5fbdaf4fb876886e4a2
Razhatawka <nataliia85sakhnova@gmail.com>
2022-11-13 21:09:25 +0200

    пытаюсь добавить секса

--- week19,JS6,task2 - cats/index.js
+++ week19,JS6,task2 - cats/index.js
@@ -1,11 +1,7 @@
-// Помните наше практическое задание с опросником для владельцев котиков?
-// Он нам снова пригодится!
 // Напишите класс Cat со свойствами, соответствующими полям вашего опросника (например, кличка, корм и пр.),
 // и по нажатию на кнопку "Ок" создайте экземпляр этого класса, заполнив его свойства ответами из опросника.
 // В этом классе будут только свойства и конструктор, без методов. 
 // Выведите результат (заполненный объект класса Cat) в консоль при помощи console.log.
-
-
 class Cat {
     constructor(ownername, catname, breed, food) {
         this.ownername = ownername;
@@ -16,14 +12,19 @@ class Cat {
     }
 }
 
-
 // создаем кнопку:
 document.getElementById("button").addEventListener("click", () => {
 
     // создаем переменные, вкладываем в них значение инпутов:
     const ownername = document.getElementById("ownername").value;
     const petsname = document.getElementById("petsname").value;
-    // const sex = document.getElementById("sex").value; //как сделать с радиокнопкой?
+    // if (document.getElementById("boy") == checked) {
+    //     const sex = "boy";
+    // } else {
+    //     const sex = "girl";
+    // }
+    const selected = document.querySelector('input[name="sex"]:checked').value;
+    console.log(selected);
     const breed = document.getElementById("breed").value;
     const food = document.getElementById("food").value;
 
@@ -31,9 +32,8 @@ document.getElementById("button").addEventListener("click", () => {
     const specificcat = new Cat(ownername, petsname, breed, food);
     console.log(specificcat);
 
-    // document.getElementById("plasefornewcat").innerHTML(specificcat); - думаю что надо так, но не работает
-
-    const cat_container = document.createElement("p"); //тоже не работает, но хоть не ругается
-    cat_container.textContent = specificcat;
+    // document.getElementById("plasefornewcat").innerHTML(specificcat);
+    // document.getElementById("plasefornewcat").textContent = specificcat;
+    // document.createElement("p").innerHTML(specificcat);
 
 })
