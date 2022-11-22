c3a296945c306ac69c350d6d6ff3810faf187c82
Razhatawka <nataliia85sakhnova@gmail.com>
2022-11-22 21:02:50 +0200

    написала кнопки

--- week19, js6, task4 - validator/index.js
+++ week19, js6, task4 - validator/index.js
@@ -8,15 +8,19 @@
 
 // Сделайте 2 версии этого класса - стандартную и статическую.
 
+//словарик:
+const email = document.getElementById("Email").value;
+const regexpEmail = /^(([^<>()[\].,;:\s@"]+(\.[^<>()[\].,;:\s@"]+)*)|(".+"))@(([^<>()[\].,;:\s@"]+\.)+[^<>()[\].,;:\s@"]{2,})$/iu;
+
+const buttonNormal = document.getElementById("buttonNormal");
+const buttonStatic = document.getElementById("buttonStatic");
 class Validator {
     isEmail() {
-        const email = document.getElementById("Email").value;
-        const regexpEmail = /^(([^<>()[\].,;:\s@"]+(\.[^<>()[\].,;:\s@"]+)*)|(".+"))@(([^<>()[\].,;:\s@"]+\.)+[^<>()[\].,;:\s@"]{2,})$/iu;
         if {
             email == str.match(regexpEmail);
         } else {
             document.getElementById("alarm").textContent = "Проверь написание Email";;
-        }
+        };
     }
 
     isDomain() {};
@@ -31,18 +35,19 @@ class Validator {
     static isPhone() {};
 }
 
+buttonNormal.addEventListener("click", () => { //вынесенно в функцию для отсрачки события
+    //стандартная вызывается вот так
+    var validator = new Validator();
+    console.log(validator.isEmail('alisa@mail.ru'));
+    console.log(validator.isDomain('itgirlschool.ru'));
+    console.log(validator.isDate('12.05.2021'));
+    console.log(validator.isPhone('+7(910)123-45-67')); //тут используем формат своей страны
+})
 
-вызов по кнопке как в предыдущем задании
-//стандартная вызывается вот так
-var validator = new Validator();
-console.log(validator.isEmail('alisa@mail.ru'));
-console.log(validator.isDomain('itgirlschool.ru'));
-console.log(validator.isDate('12.05.2021'));
-console.log(validator.isPhone('+7(910)123-45-67')); //тут используем формат своей страны
-
-вызов по кнопке как в предыдущем задании
-//а статическая так
-console.log(ValidatorStatic.isEmail('alisa@mail.ru'));
-console.log(ValidatorStatic.isDomain('itgirlschool.ru'));
-console.log(ValidatorStatic.isDate('12.05.2021'));
-console.log(ValidatorStatic.isPhone('+7(910)123-45-67')); //тут используем формат своей страны
+buttonStatic.addEventListener("click", () => { //вынесенно в функцию для отсрачки события
+    //а статическая так
+    console.log(ValidatorStatic.isEmail('alisa@mail.ru'));
+    console.log(ValidatorStatic.isDomain('itgirlschool.ru'));
+    console.log(ValidatorStatic.isDate('12.05.2021'));
+    console.log(ValidatorStatic.isPhone('+7(910)123-45-67')); //тут используем формат своей страны
+})
