e9400cedd75c7009de2cba3c45e9a56b1d310ed9
Razhatawka <nataliia85sakhnova@gmail.com>
2023-01-18 08:39:27 +0200

    /

--- swap/index.js
+++ swap/index.js
@@ -5,17 +5,17 @@
 function swap(obj) {
     const res = {};
 
-    for (let key in obj) { //тут наверное доложен быль форыч
-        obj[`${key}`] = res[`${value}`];
-        obj[`${value}`] = res[`${key}`];
+    for (let key in obj) {
+        // console.log(key); // smile (ключ!)
+        // console.log(res[key]); // undefined (значение!)
+        // res[key] = myObj[key]; // создаем ключ smile в объекте res, кладем в него значение '😃' ключа smile из объекта myObj
+        // console.log(myObj[key]);
 
-        // let key = obj[value];
-        // res[key] = value;
-    }
-
-    // добавьте в res ключи и значения obj, поменяв их местами
-
-    return res;
+        res[key] = obj[key];
+        console.log(key); //smile
+        console.log(obj[key]); // 😃
+        console.log(res[key]); //smile
+    };
 }
 
 const myObj = {
@@ -27,5 +27,52 @@ const myObj = {
     dizziness: '😵'
 };
 
-console.log(myObj); // { first: 1, second: 2, third: 3 }
-console.log(swap(myObj)); // { 1: "first", 2: "second", 3: "third" }
+swap(myObj);
+
+
+//     for (let key in obj) {
+//         obj[`${key}`] = res.key {};
+//         console.log(res.key.value);
+//         // obj[`${key}`].value = res[`${key}`];
+
+//         // как в объекте объявить ключ и присоединить к нему значение:
+//         // res[`${key}`] = true;
+//         // res.key = [`${value}`];
+
+//     // добавьте в res ключи и значения obj, поменяв их местами
+
+//     return res;
+// }
+
+// const res = {
+//     "s": "h",
+// };
+// let x;
+// x = 3;
+
+
+// for (let key in myObj) {
+//     console.log(key); // smile (ключ!)
+//     console.log(res[key]); // undefined (значение!)
+//     res[key] = myObj[key]; // создаем ключ smile в объекте res, кладем в него значение '😃' ключа smile из объекта myObj
+//     console.log(myObj[key]);
+//     console.log(res[key]);
+// };
+
+// console.log(myObj[`smirk`]);
+//как вывести ключ
+// myObj 
+// res
+
+// как вывести значение ключа
+// myObj 
+// res
+
+
+// console.log(res);
+
+
+// console.log(myObj.smile);
+
+// console.log(myObj); // { first: 1, second: 2, third: 3 }
+// console.log(swap(myObj)); // { 1: "first", 2: "second", 3: "third" }
