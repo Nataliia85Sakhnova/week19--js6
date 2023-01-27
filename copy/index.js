0d903879c6f9f579d22d32fbd90217fc1c58fa7d
Razhatawka <nataliia85sakhnova@gmail.com>
2023-01-27 19:09:22 +0200

    пока не работает

--- copy/index.js
+++ copy/index.js
@@ -4,12 +4,12 @@
 //  Небольшая подсказка: при создании объекта - копии вам поможет метод `Object.keys()`.
 
 function copy(obj) {
-    // ДУМАЕМ СЕЙЧАС СЮДА:
     const newObj = Object.assign({}, obj);
-    // Этот код скопировал все свойства объекта firstObj в пустой объект.
-    // После копирования ссылка на новый объект была записана в переменную newObj. 
-    console.log(newObj === obj); // false
-    // напишите свой код здесь
+    // const mergedObject = {
+    //     ...obj
+    // };
+    // console.log(mergedObject);
+    // return
 }
 
 const firstObj = {
@@ -23,10 +23,11 @@ const thirdObj = copy(firstObj);
 
 // console.log(firstObj); // { one: 1, two: 2, three: 3 }
 // console.log(secondObj); // { one: 1, two: 2, three: 3 }
-// console.log(thirdObj); // { one: 1, two: 2, three: 3 }
+console.log(thirdObj); // { one: 1, two: 2, three: 3 }
 
-// firstObj.four = 4;
+firstObj.four = 4;
 
 // console.log(firstObj); // { one: 1, two: 2, three: 3, four: 4 }
-// console.log(secondObj); // { one: 1, two: 2, three: 3, four: 4 }// thirdObj не изменился
-// console.log(thirdObj); // { one: 1, two: 2, three: 3 }
+// console.log(secondObj); // { one: 1, two: 2, three: 3, four: 4 }
+// thirdObj не изменился
+console.log(thirdObj); // { one: 1, two: 2, three: 3 }
